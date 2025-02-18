00a4ef838d9cb58f042c1dc125ddd3671727d340
Nikola <nikola88393@gmail.com>
2025-02-18 17:02:52 +0200

    Add route param to route middleware

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const notesRouter = require('./routes/notesRoute');
 app.use(express.json());
 app.use(cors());
 
-app.use(notesRouter);
+app.use('/', notesRouter);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
