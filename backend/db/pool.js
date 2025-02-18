720eec2772c87002465bbcb905fc2f6c39ba76a9
Nikola <nikola88393@gmail.com>
2025-02-18 16:59:29 +0200

    Update db connection file to use env variables

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,8 +1,10 @@
+const dotenv = require('dotenv').config()
 const {Pool} = require('pg')
 
+console.log(process.env.DB_PASS);
 module.exports = new Pool({
-    database: 'postgres',
-    host: 'localhost',
-    user: 'postgres',
-    password: "test123"
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
 })
